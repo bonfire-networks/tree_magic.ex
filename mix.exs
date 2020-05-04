a5fbeeea9b6feb1c98621ecf7b8cbbbc7f2c514a
Antonis Kalou <kalouantonis@protonmail.com>
2020-05-04 10:47:09 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TreeMagic.MixProject do
   def project do
     [
       app: :tree_magic,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       compilers: [:rustler] ++ Mix.compilers(),
